b7332d963a37e5b38019c2b009eee67f7ca58ff4
RavenCha <113488948+RavenCha@users.noreply.github.com>
2022-09-26 19:22:23 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Raven
+ * Date: 9/25/2022
+ * Time: 2:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace EngagingActivityStruct
+{
+	class Program
+	{
+		struct Employee
+{
+    public string firstName;
+    public string lastName;
+    public int age;
+    public string completedTraining;
+}
+		public static void Main(string[] args)
+		{
+		
+			
+	    Console.Title = "Struct";
+        Employee employee1;
+        Console.WriteLine("============");
+        employee1.firstName = "Juan";
+        employee1.lastName = "Dela Cruz";
+        employee1.age = 25;
+        employee1.completedTraining = "Yes";
+      
+
+        Console.WriteLine("Name: "+employee1.firstName);
+        Console.WriteLine("Last Name: "+employee1.lastName);
+        Console.WriteLine("Age: "+employee1.age);
+        Console.WriteLine("Completed Training: "+employee1.completedTraining);
+        Console.WriteLine("============");
+        Console.Read();
+        
+				
+		}
+	}
+}
